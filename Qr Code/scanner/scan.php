54eb2bf2b6a97de2508d1f0c86f710aee27ff3d9
LeJehanno <98598506+LeJehanno@users.noreply.github.com>
2022-03-03 19:19:37 +0100

    Add files via upload

--- Qr Code/scanner/scan.php
+++ Qr Code/scanner/scan.php
@@ -2,39 +2,36 @@
     <head>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="font.css">
+        <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     </head>
     
     <body>
         <div class="container">
-            <div class="ligne">
                 <div class="un">
-                    <video id="preview" width="50%"></video>
+                    <video id="cam" width="10%" controls autoplay></video>
                 </div>
-                <div class="un">
-                    <label>QR Code</label>
-                    <imput type="text" name="text" id="text" readonyy="" placeholder="scan QR Code" class="form-controle"></imput>  
+                <div class="deux">
+                    <label>Code :</label>
+                    <input type="text" name="text" id="text" readonyy="" placeholder="IMAT" class="form-control"></input>  
                 </div>
-            </div>
         </div>
         
-       <script src="vglnk" href="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" rel="nofollow"></script>
+       
         <script type="text/javascript">
-            let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 1, mirror: false });
+            let scanner = new Instascan.Scanner({ video: document.getElementById('cam'), scanPeriod: 1, mirror: false });
             Instascan.Camera.getCameras().then(function(cameras){
                 if ( cameras.length > 0){
-                    scanner.start(cameras[0]);
-                
-            } 
-                else {
-                    alert(' Pas de caméras trouvé ');
+                    scanner.start(cameras[0]);     
+            }  else {
+                    alert(' Pas de camera ');
                 }
             }). catch (function(e) {
                  console.error(e);
             });
             
-            scanner.addListener('scan', function(c)){
-                document.getDocumentById('text').value-c;
+            scanner.addListener('scan', function(c){
+                document.getElementById('text').value=c;
                 document.forms[0].submit();    
             });
         </script>
