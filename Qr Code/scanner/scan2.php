66d544ee1b9e305249d772e5235cbe2d656dba16
LeJehanno <98598506+LeJehanno@users.noreply.github.com>
2022-03-04 09:27:23 +0100

    Create scan2.php

---
+++ Qr Code/scanner/scan2.php
@@ -0,0 +1,87 @@
+<html>
+
+    <head>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+
+        <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+
+        
+
+        <link rel="stylesheet" type="text/css" >
+
+    </head>
+
+    
+
+    <body>
+
+          
+
+        <div class="container">
+
+                <div class="un">
+
+                    <?php include 'img.php'?>
+
+                     <img  src="http://172.20.233.38/command/inquiry.cgi?ind=camera" id="cam" width="10%" height="10%" controls autoplay></img> 
+
+                   <!--  <iframe name="frame" src="http://172.20.233.38/oneshotimage1?1646380138519" id="cam" width="100%" height="100%"></iframe>-->
+
+
+
+                </div>
+
+                <div class="deux">
+
+                    <label>Code :</label>
+
+                    <input type="text" name="text" id="text" readonyy="" placeholder="IMAT" class="form-control"></input>  
+
+                </div>
+
+        </div>
+
+        
+
+       
+
+        <script type="text/javascript">
+
+            let scanner = new Instascan.Scanner({ video: document.getElementById('cam'), scanPeriod: 1, mirror: false });
+
+            Instacan.Camera.getCameras().then(function(cameras){
+
+                if ( cameras.length > 0){
+
+                    scanner.start(cameras[0]);     
+
+            }  else {
+
+                    alert(' Pas de camera ');
+
+                }
+
+            }). catch (function(e) {
+
+                 console.error(e);
+
+            });
+
+            
+
+            scanner.addListener('scan', function(c){
+
+                document.getElementById('text').value=c;
+
+                document.forms[0].submit();    
+
+            });
+
+        </script>
+
+    </body>
+
+</html>
