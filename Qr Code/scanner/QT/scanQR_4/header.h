57ae3aa7576f18fff0d3742a6ecbdd69b1fc12d7
LeJehanno <98598506+LeJehanno@users.noreply.github.com>
2022-04-01 16:15:58 +0200

    Create header.h

---
+++ Qr Code/scanner/QT/scanQR_4/header.h
@@ -0,0 +1,33 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+// qr
+#include <zbar.h>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#include <QCoreApplication>
+#include <QtCore>
+
+//bdd
+#include <QtSql>
+#include <QSqlDatabase>
+#include "/home/pi/LibMysql/libs/sqlrequest.h"
+#include <QtSql/QSqlQuery>
+
+//panneau
+#include <QUdpSocket>
+#include <QDebug>
+#include <QString>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#endif // HEADER_H
