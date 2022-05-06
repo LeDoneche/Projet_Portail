47c3aca87a94bacd6bd54e6861b7475f8647e21a
LeJehanno <98598506+LeJehanno@users.noreply.github.com>
2022-05-06 13:52:35 +0200

    Update main.cpp

--- Qr Code/scanner/QT/scanQR7/main.cpp
+++ Qr Code/scanner/QT/scanQR7/main.cpp
@@ -43,8 +43,36 @@ int main()
              Affichage nombre de places restante
      ************************************************************/
     PanneauLumineux *pl = new PanneauLumineux();
-    QString message = "Places: " +  QString::number(co1->nbPlace) + "/50";
-    pl->afficher(message);
+    
+    //Message pour la limite de place
+    if (co1->nbPlace >= 50) {
+        QString message4 = "Parking Plein";
+        pl->afficher(message4);
+    } else {
+        /*
+           je récupère la variable "nb" et je l'insère dans le message
+           pour afficher nombres de places dispo
+          */
+        QString message = "Places: " + QString::number(co1->nbPlace) +"/50";
+        pl->afficher(message);
+        
+        //temporisation de 3s
+        QThread::sleep(3);
+        
+        //si qrcode non présent dans la bdd
+        if( co1->verifChampBdd == 0) {
+            QString message2 = "QRCode Faux";
+            pl->afficher(message2);
+            QThread::sleep(3);
+            pl->afficher(message); //je réaffiche le nombres de places restantes
+        } else  {
+            //si qrcode présent dans la bdd
+            QString message3 = "QRCode Bon";
+            pl->afficher(message3);
+            QThread::sleep(3);
+            pl->afficher(message); //je réaffiche le nombres de places restantes
+        }
+    }
 
 
     return 0;
